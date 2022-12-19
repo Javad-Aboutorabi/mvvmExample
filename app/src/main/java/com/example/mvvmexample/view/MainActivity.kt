e7e9ff386f444307a6f5445e7554f811d07fc981
javad <javad.aboutorabi@gmail.com>
2022-12-19 21:21:59 +0330

    feat: create SaveActivity

--- app/src/main/java/com/example/mvvmexample/view/MainActivity.kt
+++ app/src/main/java/com/example/mvvmexample/view/MainActivity.kt
@@ -1,6 +1,7 @@
-package com.example.mvvmexample
+package com.example.mvvmexample.view
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
@@ -8,13 +9,11 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import android.widget.Button
 import android.widget.EditText
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
+import com.example.mvvmexample.R
 //For resource
 import com.example.mvvmexample.viewmodels.MainViewModel
-import com.example.mvvmexample.models.NicePlace
 import com.example.mvvmexample.adapters.RecyclerAdapter
 import com.example.mvvmexample.viewmodels.MainViewModelFactory
 
@@ -23,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
     private lateinit var mainrecycler: RecyclerView
     private lateinit var submitButton: Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,9 +31,11 @@ class MainActivity : AppCompatActivity() {
         val factory = MainViewModelFactory()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         submitButton = findViewById(R.id.button)
+        val openSaveActivity = findViewById<Button>(R.id.open_save_activity)
         submitButton.setOnClickListener {
             addData()
         }
+        openSaveActivity.setOnClickListener { startActivity(Intent(this,SaveActivity::class.java)) }
 
 
 
